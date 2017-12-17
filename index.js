1780b75938eab3906f95436d1fb8a4e904aed70c
derrick-long <long.derrick8@gmail.com>
2017-12-17 15:32:47 -0500

    Add simple error messages

--- index.js
+++ index.js
@@ -5,20 +5,43 @@ function isEmail(email) {
 
 
 $('#submit-button').click(function(){
-  var errorMessage = "";
+  var errorMessage = "<p> Please correct the following: </p> ";
+  var fieldMissing = "";
+
+  if ($('#email').val() == "") {
+    fieldMissing += "<br> Email is blank";
+  }
 
   if (isEmail($('#email').val()) == false) {
     errorMessage += "<p> Email is invalid! </p>";
+  }
+
+
+  if ($('#telephone').val() == "") {
+      fieldMissing += "<br> Telephone Number is blank";
   }
 
   if ($.isNumeric($('#telephone').val()) == false) {
     errorMessage += "<p> Telephone number is invalid! </p>";
   }
 
+
+  if ($('#password').val() == "") {
+      fieldMissing += "<br> Password is blank";
+  }
+
+  if ($('#confirm-password').val() == "") {
+      fieldMissing += "<br> Password confirmation is blank";
+  }
+
   if ($('#password').val() !=  $('#confirm-password').val()) {
     errorMessage += "<p> Passwords do not match! </p>";
   }
 
-  alert(errorMessage);
+  if (fieldMissing != "") {
+    errorMessage += fieldMissing;
+  }
+
+  $('.errors').append(errorMessage);
 
 });
